5c4e67fc965df4831c34abb09611cf0511a68ac7
Peter B <pbrunner@gmail.com>
2011-04-09 16:34:41 -0400

    updated readme. cleaned up output while generating/tagging.

--- beetsplug/replaygain/__init__.py
+++ beetsplug/replaygain/__init__.py
@@ -3,14 +3,21 @@ from beets import autotag, ui
 from beets.plugins import BeetsPlugin
 from beets.ui import print_, Subcommand
 
+import sys
+import os.path
+import gobject
 
 import pygst
 pygst.require('0.10')
 import gst
 
+from rgain import rgio
+from rgain.script import ou, un, Error, common_options
+
+
 from collections import defaultdict
 from rgain import rgio
-from rgain.script.replaygain import do_gain
+#from rgain.script.replaygain import do_gain
 
 import os, logging
 
@@ -22,29 +29,129 @@ DEFAULT_REFERENCE_LOUDNESS = 89
 DEFAULT_MP3_FORMAT = 'fb2k'
 DEFAULT_NO_ALBUM = False
 
+verbose = False
 
 class ReplayGainPlugin(BeetsPlugin):
-	'''Provides replay gain analysis for beets'''
+    '''Provides replay gain analysis for beets'''
 
-	def __init__(self):
-		self.register_listener('loaded', self.loaded)
-		self.register_listener('album_imported', self.album_imported)
+    def __init__(self):
+        self.register_listener('album_imported', self.album_imported)
 
-	def configure(self, config):
-		self.ref_level =ui.config_val(config, 'replaygain', 'reference_loundess', DEFAULT_REFERENCE_LOUDNESS, int)
-		self.mp3_format = ui.config_val(config, 'replaygain', 'mp3_format', DEFAULT_MP3_FORMAT)
-		#self.no_album = ui.config_val(config, 'replaygain', 'no_album', DEFAULT_NO_ALBUM, bool)
+    def configure(self, config):
+        self.ref_level =ui.config_val(config, 'replaygain', 'reference_loundess', DEFAULT_REFERENCE_LOUDNESS, int)
+        self.mp3_format = ui.config_val(config, 'replaygain', 'mp3_format', DEFAULT_MP3_FORMAT)
+        #self.no_album = ui.config_val(config, 'replaygain', 'no_album', DEFAULT_NO_ALBUM, bool)
+    
+    def album_imported(self, album):
+        force = True
+        dry_run = False        
+        is_album = True
+        verbose = False
+        
+        print_("Tagging Replay Gain:  %s - %s" % (album.albumartist, album.album))
+        
+        item_paths = [item.path for item in album.items()]
+        do_gain(item_paths, self.ref_level, force, dry_run, is_album, self.mp3_format )
 
-	def loaded(self):
-		pass
+# calculate the gain for the given files
+def calculate_gain(files, ref_level):
+    # this has to be done here since Gstreamer hooks into the command line
+    # arguments if it's imported on module level
+    from rgain import rgcalc
 
-	def album_imported(self, album):
-		force = True
-		dry_run = False
-		ignore_cache = False
-		is_album = True
+    # handlers
+    def on_finished(evsrc, trackdata, albumdata):
+        loop.quit()
 
-		item_paths = [item.path for item in album.items()]
+    def on_trk_started(evsrc, filename):
+        if verbose: print_( ou("  %s:" % filename.decode("utf-8")))
 
-		do_gain(item_paths, self.ref_level, force, dry_run, is_album, self.mp3_format )
+    def on_trk_finished(evsrc, filename, gaindata):
+        if gaindata:
+            if verbose: print_( "%.2f dB" % gaindata.gain)
+        else:
+            if verbose: print_( "done" )
 
+    rg = rgcalc.ReplayGain(files, True, ref_level)
+    rg.connect("all-finished", on_finished)
+    rg.connect("track-started", on_trk_started)
+    rg.connect("track-finished", on_trk_finished)
+    loop = gobject.MainLoop()
+    rg.start()
+    loop.run()
+    return rg.track_data, rg.album_data
+
+
+def do_gain(files, ref_level=89, force=False, dry_run=False, album=True,
+            mp3_format="ql"):
+    
+    files = [un(filename, sys.getfilesystemencoding()) for filename in files]
+    
+    formats_map = rgio.BaseFormatsMap(mp3_format)
+    
+    newfiles = []
+    for filename in files:
+        if not os.path.splitext(filename)[1] in formats_map.supported_formats:
+            if verbose: print_( ou(u"%s: not supported, ignoring it" % filename))
+        else:
+            newfiles.append(filename)
+    files = newfiles
+    
+    if not force:
+        if verbose: print_( "Checking for Replay Gain information ...")
+        newfiles = []
+        for filename in files:
+            if verbose: print_( ou(u"  %s:" % filename))
+            try:
+                trackdata, albumdata = formats_map.read_gain(filename)
+            except Exception, exc:
+                raise Error(u"%s: error - %s" % (filename, exc))
+            else:
+                if trackdata and albumdata:
+                    if verbose: print_( "track and album")
+                elif not trackdata and albumdata:
+                    if verbose: print_( "album only")
+                    newfiles.append(filename)
+                elif trackdata and not albumdata:
+                    if verbose: print_( "track only")
+                    if album:
+                        newfiles.append(filename)
+                else:
+                    if verbose: print_( "none")
+                    newfiles.append(filename)
+        
+        if not album:
+            files = newfiles
+        elif not len(newfiles):
+            files = newfiles
+    
+    if not files:
+        # no files left
+        if verbose: print_( "Nothing to do.")
+        return 0
+    
+    # calculate gain
+    if verbose: print_( "Calculating Replay Gain information ...")
+    try:
+        tracks_data, albumdata = calculate_gain(files, ref_level)
+        if album:
+            if verbose: print_( "  Album gain: %.2f dB" % albumdata.gain)
+    except Exception, exc:
+        raise Error(u"Error while calculating gain - %s" % exc)
+    
+    if not album:
+        albumdata = None
+    
+    # write gain
+    if not dry_run:
+        if verbose: print_( "Writing Replay Gain information to files ...")
+        for filename, trackdata in tracks_data.iteritems():
+            if verbose: print_( ou(u"  %s:" % filename))
+            try:
+                formats_map.write_gain(filename, trackdata, albumdata)
+            except Exception, exc:
+                raise Error(u"%s: error - %s" % (filename, exc))
+            else:
+                if verbose: print_( "done")
+    
+    if verbose: print_( "Done" )
